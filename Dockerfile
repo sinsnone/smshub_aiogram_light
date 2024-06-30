ec70463217bb4bf32278d38672dce176d2535ad8
sinsnone <160766329+sinsnone@users.noreply.github.com>
2024-06-30 07:48:53 +0200

    ...

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,13 @@
-FROM python:3.11-alpine
+FROM python:3.9
 
-WORKDIR /app
+RUN apt-get update -y && apt-get install -y --no-install-recommends ffmpeg \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+COPY . /app/
+WORKDIR /app/
 
-RUN pip install --upgrade pip
-COPY ./requirements.txt /app
-RUN pip install --no-cache-dir -r /app/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r requirements.txt
 
-COPY . .
-
-CMD python main.py
+EXPOSE 8080
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -8,14 +8,30 @@ import sys
 from aiogram import executor
 
 from telegram.bot import dp
+from aiohttp import web
+
+
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+async def init_web_server():
+    server = web.Application()
+    server.router.add_get('/', handle)
+    runner = web.AppRunner(server)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8080)
+    await site.start()
+    
+
 
 if __name__ == '__main__':
+    await init_web_server()
     # Set up logging configurations
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         stream=sys.stdout,
     )
-
+    
     # Start the bot
     executor.start_polling(dp, skip_updates=True)
